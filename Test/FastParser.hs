d8cb4ffc50534f451a5a9260c51b075d98785976
Frederik Hanghøj Iversen <me@fredefox.eu>
2014-10-31 16:51:49 +0100

    Add Test-module

---
+++ Test/FastParser.hs
@@ -0,0 +1,24 @@
+{-
+ - Testing "Fast"-Parser
+ - =====================
+ -
+ - This module implements pretty printing Fast-expressions.
+ -
+ - NB: This is not really a part of the library and should maybe be placed in
+ - some auxillary place.
+ -
+ - Written by Frederik Hanghøj Iversen
+ - for the course Advanced Programming
+ - at The University of Copenhagen 2014
+ -
+ -     me@fredefox.eu /^._
+ -      ,___,--~~~~--' /'~
+ -      `~--~\ )___,)/'
+ -          (/\\_  (/\\_
+ -
+ -}
+module Test.FastParser () where
+
+import FastParser
+import FastPrinter
+import Test.QuickCheck
